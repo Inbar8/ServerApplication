b34a6519f0c410b9042cf106541c3aad8326fb5f
ronbenshimol <ronbenshimol@gmail.com>
2019-01-03 15:10:55 +0200

    1

---
+++ IServer.h
@@ -0,0 +1,20 @@
+#ifndef SERVERAPPLICATION_ISERVER_H
+#define SERVERAPPLICATION_ISERVER_H
+
+namespace server_side{
+
+    class IServer{
+
+        virtual void open()=0;
+
+
+
+    };
+
+
+
+}
+
+
+
+#endif //SERVERAPPLICATION_ISERVER_H
