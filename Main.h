fe036fabf3ff9e2940b6dd98a3c0ac4102f9fe5e
Shaked Cohen <shkcohen@gmail.com>
2019-01-06 16:07:05 +0200

    part 1 structure

--- Main.h
+++ Main.h
@@ -4,6 +4,8 @@
 #define SERVERAPPLICATION_MAIN_H
 
 #include <string>
+#include "StringReverser.h"
+#include "MySerialServer.h"
 
 namespace boot {
 
@@ -18,8 +20,12 @@ namespace boot {
 
             int port = std::stoi(argv[0]);
 
+            auto stringSolver = new server_side::StringReverser();
+            auto serialServer = new server_side::MySerialServer();
+            
 
 
+            using namespace boot;
         }
 
 
